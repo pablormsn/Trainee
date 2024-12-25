328c038067ddacb5acf7a7fb24cfbf7fa9b4e519
pablormsn <problesmansilla@uma.es>
2024-12-25 20:05:30 +0100

    boceto de hub

--- app/src/main/java/com/coral/fithub/MainActivity.kt
+++ app/src/main/java/com/coral/fithub/MainActivity.kt
@@ -2,29 +2,22 @@ package com.coral.fithub
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val usernameEditText = findViewById<EditText>(R.id.et_username)
-        val passwordEditText = findViewById<EditText>(R.id.et_password)
-        val loginButton = findViewById<Button>(R.id.btn_login)
+        // Botones
+        val gymButton: ImageButton = findViewById(R.id.gymButton)
 
-        loginButton.setOnClickListener {
-            val username = usernameEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            if (username.isNotEmpty() && password.isNotEmpty()) {
-                // Aquí podrías verificar credenciales o navegar a otra actividad
-                Toast.makeText(this, "Bienvenido $username", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
-            }
+        // Eventos de clic
+        gymButton.setOnClickListener {
+            Toast.makeText(this, "Gimnasio seleccionado", Toast.LENGTH_SHORT).show()
         }
     }
 }
