ac69048999a9042cd06ed256ed81a9d28e77eb1b
Pablorgu <pablor623@gmail.com>
2024-12-26 18:47:18 +0100

    arreglo problemilla modelos

--- app/src/main/java/com/coral/fithub/data/model/Entrenamiento.kt
+++ app/src/main/java/com/coral/fithub/data/model/Entrenamiento.kt
@@ -1,7 +1,19 @@
 package com.coral.fithub.data.model
 
+import androidx.room.*
 import java.time.LocalDate
 
+@Entity(
+    tableName = "Entrenamiento",
+    foreignKeys = [
+        ForeignKey(
+            entity = Rutina::class,
+            parentColumns = ["idRutina"],
+            childColumns = ["idRutina"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Entrenamiento (
     val idEntrenamiento: Int,
     val fechaRealizacion: LocalDate,
